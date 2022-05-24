9b5b5c86116b247c8b52de2211218315c0a79fcb
Suman <spokhrel196@gmail.com>
2022-05-24 20:40:09 +0545

    NavBar component completeed

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,28 +1,39 @@
-import React,{createContext, useContext, useState} from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
 const StateContext = createContext();
 
 const intialState = {
-     chat:false,
-     cart:false,
-     userProfile:false,
-     notification:false,
+     chat: false,
+     cart: false,
+     userProfile: false,
+     notifications: false,
 }
 
-export const ContextProvider = ({children})=>{
-     
+export const ContextProvider = ({ children }) => {
+
      const [activeMenu, setActiveMenu] = useState(true);
-     
+     const [isClicked, setIsClicked] = useState(intialState);
+     const [screenSize, setScreenSize] = useState(undefined)
+
+     const handleClick = (clicked)=>{
+          setIsClicked({...intialState, [clicked]:true})
+     }
+
      return (
-          <StateContext.Provider 
-          value={{
-               activeMenu,
-               setActiveMenu,
-          }}
+          <StateContext.Provider
+               value={{
+                    activeMenu,
+                    setActiveMenu,
+                    isClicked,
+                    setIsClicked,
+                    handleClick,
+                    screenSize,
+                    setScreenSize
+               }}
           >
                {children}
           </StateContext.Provider>
      )
 }
 
-export const useStateContext = ()=>useContext(StateContext)
+export const useStateContext = () => useContext(StateContext)
