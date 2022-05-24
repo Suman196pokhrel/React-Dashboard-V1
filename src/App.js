cfcf104b77ccffd7a6378975858cca7d0490bfdc
Suman <spokhrel196@gmail.com>
2022-05-24 23:28:43 +0545

    Revenu section completed

--- src/App.js
+++ src/App.js
@@ -98,7 +98,7 @@ const App = () => {
             <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
               <Navbar />
             </div>
-          </div>
+         
 
           <div>
             <Routes>
@@ -131,6 +131,8 @@ const App = () => {
 
 
             </Routes>
+          </div>
+
           </div>
 
 
